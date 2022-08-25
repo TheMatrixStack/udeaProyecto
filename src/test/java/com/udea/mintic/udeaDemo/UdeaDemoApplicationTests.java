b7f826bd4ffd2168597d7b87c9a265f10a86b9d5
csduque3 <65147055+csduque3@users.noreply.github.com>
2022-08-24 20:36:16 -0500

    Initial commit

---
+++ src/test/java/com/udea/mintic/udeaDemo/UdeaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.mintic.udeaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdeaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
